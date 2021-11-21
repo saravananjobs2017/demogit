0ddd6c53d27bc4ce565f2eaae45e77ce84d440e8
Saravanan Dhanapal <saravanan.dhanapal@tesco.com>
2021-11-21 11:40:09 +0530

    demogit project

---
+++ Desktop/demogit/src/test/java/com/saravanan/demo/demogit/DemogitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saravanan.demo.demogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemogitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
